04f5700543fbd17e987643c4b4d81c3e475dcae3
huudungg <huudung2004kt@gmail.com>
2024-09-15 12:40:52 +0700

    upload code, setup lib, setup service

---
+++ src/service/axios.custom.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+// Set config defaults when creating the instance
+const instance = axios.create({
+    baseURL: import.meta.env.VITE_BACK_END_URL
+});
+
+// Add a request interceptor
+instance.interceptors.request.use(function (config) {
+    if (typeof window !== "undefined" && window && window.localStorage &&
+        window.localStorage.getItem('access_token')) {
+        config.headers.Authorization = 'Bearer ' + window.localStorage.getItem('access_token');
+    }
+    // Do something before request is sent
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
+
+instance.interceptors.response.use(function (response) {
+    if (response.data && response.data.data) {
+        return response.data
+    }
+    return response;
+}, function (error) {
+    if (error.response && error.response.data) return error.response.data
+});
+
+export default instance
